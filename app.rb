1ae2d90ae4e6f6c6cb54f720693f51b39ea962b4
William J Teskey <willjteskey@gmail.com>
2019-02-21 20:53:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,14 +6,16 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-  original_string = params["string"]
-  @reversed_string = original_string.reverse
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
 
-  erb :reversed
-end
+    erb :reversed
+  end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
